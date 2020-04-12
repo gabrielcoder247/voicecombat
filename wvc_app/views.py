92b4cd68e104861e4b4857f6cb132c1658c9a414
gabriel <gabrielcoder247@gmail.com>
2020-04-12 00:42:07 +0000

    craeting the views functions

--- wvc_app/views.py
+++ wvc_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+def home(request):
+
+    '''
+    View home function that returns the home page
+    '''
+  
+    voice_card = VoiceCard.objects.all()
+    print(voice_card)
+
+    apartments = Listing.objects.filter(category__contains="apartments").all()
+    print(apartments)
+
+    mansionattes = Listing.objects.filter(category__contains="mansionattes").all()
+    print(mansionattes)
+
+    bungalows = Listing.objects.filter(category__contains="bungalows").all()
+    print(bungalows)
+    
+
+    
+    return render(request, 'home.html', {"listings":listings, "apartments": apartments,"mansionattes":mansionattes,"bungalows":bungalows})
